f0f203426834d4cc0fa56a8aee4ef3870e2ee2e6
“Alvaro <asutter.dev@gmail.com>
2023-02-10 06:17:47 +0100

    Add prisma ORM and connect with DB - Add prisma schema with modells - Create tables in DB - Update startApp script to start containers and prisma

--- startApp.sh
+++ startApp.sh
@@ -2,4 +2,11 @@
 # Install Node Modules
 docker-compose run backend npm install
 # Start Containers
-docker-compose up
+docker-compose up -d
+# Create tables and prisma client.
+cd backend
+echo "DATABASE_URL=\"postgresql://admin:pass@localhost:5432/flashcards?schema=public\"" > .env
+npx prisma db push
+npx prisma generate
+# Display containers logs
+docker logs -f backend
